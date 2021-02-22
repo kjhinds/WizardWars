ea8ef345c32b5e4966ac757bb19440eb50549e4e
Kalon Hinds <kjhinds@gmail.com>
2021-02-21 20:25:26 -0800

    Parse out effects and damage

--- script.js
+++ script.js
@@ -1,43 +1,59 @@
 const MANA = {
     'VOLATILITY': {
-        x: 1,
+        x: 2,
         y: 0,
         hue: 0,
-    },
-    'AIR': {
-        x: Math.SQRT2,
-        y: Math.SQRT2,
-        hue: 45,
-    },
-    'MENTAL': {
-        x: 0,
-        y: 1,
-        hue: 90,
-    },
-    'WATER': {
-        x: -Math.SQRT2,
-        y: Math.SQRT2,
-        hue: 135,
-    },
-    'STABILITY': {
-        x: -1,
-        y: 0,
-        hue: 180,
-    },
-    'EARTH': {
-        x: -Math.SQRT2,
-        y: -Math.SQRT2,
-        hue: 225,
-    },
-    'PHYSICAL': {
-        x: -1,
-        y: -1,
-        hue: 270,
+        damage: 0,
+        effect: {critical: .15},
     },
     'FIRE': {
         x: Math.SQRT2,
+        y: Math.SQRT2,
+        hue: 45,
+        damage: 25,
+        effect: {burn: 2},
+    },
+    'PHYSICAL': {
+        x: 0,
+        y: 2,
+        hue: 90,
+        damage: 0,
+        effect: {petrify: .1},
+    },
+    'EARTH': {
+        x: -Math.SQRT2,
+        y: Math.SQRT2,
+        hue: 135,
+        damage: 35,
+        effect: {none: 0},
+    },
+    'STABILITY': {
+        x: -2,
+        y: 0,
+        hue: 180,
+        damage: 0,
+        effect: {stable: .3},
+    },
+    'WATER': {
+        x: -Math.SQRT2,
         y: -Math.SQRT2,
-        hue: 315,
+        hue: -135,
+        damage: 15,
+        effect: {slow: 1},
+    },
+    'MENTAL': {
+        x: 0,
+        y: -2,
+        hue: -90,
+        damage: 0,
+        effect: {confuse: .15},
+    },
+    'AIR': {
+        x: Math.SQRT2,
+        y: -Math.SQRT2,
+        hue: -45,
+        damage: 25,
+        effect: {daze: 1},
     },
 }
 
@@ -50,80 +66,62 @@ const castSelection = document.getElementById('castSelection');
 const playerHP = document.getElementById('hp');
 const playerEffects = document.getElementById('effects');
 
+const log = document.getElementById('log');
+
 const manaNames = Object.keys(MANA);
 
 let countersPicked = 0;
 let totalPicked = 0;
 
-let player = {counter: [], attack: [], hp: 1000,};
-let enemy = {counter: [], attack: [], hp: 1000,};
+let player = {counter: [], attack: [], hp: 1000, effects: []};
+let enemy = {counter: [], attack: [], hp: 1000, effects: []};
 
 playerHP.innerHTML = `${player.hp}`;
 
-manaNames.forEach(mana => {
-    const button = document.createElement("BUTTON");
-    button.innerHTML = `${mana}`
-    button.id = `counter${mana}Button`;
-    button.style.backgroundColor = `hsl(${MANA[mana].hue},100%,50%)`
-    button.addEventListener('click', counterManaPicked);
-    counterButtons.appendChild(button);
-});
-
-manaNames.forEach(mana => {
-    const button = document.createElement("BUTTON");
-    button.innerHTML = `${mana}`
-    button.id = `cast${mana}Button`;
-    button.style.backgroundColor = `hsl(${MANA[mana].hue},100%,50%)`
-    button.addEventListener('click', spellManaPicked);
-    castButtons.appendChild(button);
-});
+function createButtons(type) {
+    manaNames.forEach(mana => {
+        const button = document.createElement("BUTTON");
+        button.innerHTML = `${mana}`
+        button.id = `${type.id}${mana}`;
+        button.style.backgroundColor = `hsl(${MANA[mana].hue},100%,50%)`
+        button.addEventListener('click', manaPicked);
+        type.appendChild(button);
+    });
+}
 
 function whoIsFirst(){
     return Math.random() < 0.5 ? 'player' : 'enemy';
 }
 
 function startGame() {
+    createButtons(counterButtons);
+    createButtons(castButtons);
     if (whoIsFirst() == 'enemy') {
         pickEnemyAction();
     }
 }
 
-function counterManaPicked() {
-    counterSelection.innerHTML += `${this.innerHTML}<br>`
-    player.counter.push(this.innerHTML);
-    countersPicked++;
-    totalPicked++;
+function manaPicked() {
+    if (this.id.includes('counter')) {
+        counterSelection.innerHTML += `${this.innerHTML}<br>`
+        player.counter.push(this.innerHTML);
+        countersPicked++;
+    } else {
+        castSelection.innerHTML += `${this.innerHTML}<br>`
+        player.attack.push(this.innerHTML);
+    }
+    totalPicked++;    
     if (totalPicked >= 6) {
-        setCounterButtons('disable');
-        setCastButtons('disable');
+        setButtonsDisabled('counterButtons',true);
+        setButtonsDisabled('castButtons',true);
     } else if (countersPicked >= 2) {
-        setCounterButtons('disable');
-    }
-
-}
-
-function spellManaPicked() {
-    castSelection.innerHTML += `${this.innerHTML}<br>`
-    player.attack.push(this.innerHTML);
-    totalPicked++;
-    if (totalPicked >= 6) {
-        setCounterButtons('disable');
-        setCastButtons('disable');
+        setButtonsDisabled('counterButtons',true);
     }
 }
 
-function setCounterButtons(status) {
-    status = status == 'disable' ? true : false;
+function setButtonsDisabled(type,status) {
     manaNames.forEach(mana => {
-        const button = document.getElementById(`counter${mana}Button`);
-        button.disabled = status;
-    });
-}
-
-function setCastButtons(status) {
-    status = status == 'disable' ? true : false;
-    manaNames.forEach(mana => {
-        const button = document.getElementById(`cast${mana}Button`);
+        const button = document.getElementById(`${type}${mana}`);
         button.disabled = status;
     });
 }
@@ -136,12 +134,10 @@ function newRound() {
     } else {
         countersPicked = 0;
         totalPicked = 0;
-        setCounterButtons('enable');
-        setCastButtons('enable');
+        setButtonsDisabled('counterButtons',false);
+        setButtonsDisabled('castButtons',false);
         castSelection.innerHTML = '';
         counterSelection.innerHTML = '';
-        playerHP.innerHTML = `${player.hp}`;
-
         player.counter = [];
         player.attack = [];
     }
@@ -159,20 +155,11 @@ function resolveSpell(mana) {
     return spell;
 }
 
-function counterEnemyMana(){
-    player.counter.forEach(element => {
-        let index = enemy.attack.indexOf(element);
+function counterMana(caster, target){
+    caster.counter.forEach(element => {
+        let index = target.attack.indexOf(element);
         if (index != -1) {
-            enemy.attack.splice(index,1);
-        }
-    });
-}
-
-function counterPlayerMana(){
-    enemy.counter.forEach(element => {
-        let index = player.attack.indexOf(element);
-        if (index != -1) {
-            player.attack.splice(index,1);
+            target.attack.splice(index,1);
         }
     });
 }
@@ -181,7 +168,7 @@ function displaySpellEffect(who, spell) {
     spell.polar = cartesian2Polar(spell.x, spell.y);
     const spellDisplay = document.getElementById(`${who}Spell`);
     spellDisplay.style.backgroundColor = 
-                `hsl(${spell.polar.angle},${spell.polar.magnitude}%,50%)`;
+                `hsl(${spell.polar.angle},${spell.polar.magnitude*25}%,50%)`;
 }
 
 function pickEnemyAction() {
@@ -204,14 +191,12 @@ function pickEnemyAction() {
 
     let spell = resolveSpell(enemy.attack);
     displaySpellEffect('enemy',spell)
-    console.table(player.attack);
-    counterPlayerMana();
-    console.table(player.attack);
-    applyPlayerSpell();
+    counterMana(enemy,player);
+    applySpell(player, enemy);
 }
 
 function cartesian2Polar(x,y){
-    const magnitude = Math.sqrt(x*x + y*y)*25;
+    const magnitude = Math.sqrt(x*x + y*y);
     const angle = Math.atan2(y, x) * (180/Math.PI);
     const polarCoord = {
         magnitude: magnitude,
@@ -220,23 +205,44 @@ function cartesian2Polar(x,y){
     return polarCoord;
 }
 
-function applyEnemySpell() {
-    let spell = resolveSpell(enemy.attack);
-    spell.polar = cartesian2Polar(spell.x, spell.y);
-    player.hp -= Math.floor(spell.polar.magnitude);
-}
-
-function applyPlayerSpell() {
-    let spell = resolveSpell(player.attack);
-    spell.polar = cartesian2Polar(spell.x, spell.y);
-    enemy.hp -= Math.floor(spell.polar.magnitude);
+function applySpell(caster, target) {
+    let cast = resolveSpell(caster.attack);
+    cast.polar = cartesian2Polar(cast.x, cast.y);
+    console.log(cast.polar.angle);
+    let spellDamage = 0;
+    let spellEffect = {};
+    manaNames.forEach(entry => {
+        let mana = MANA[entry];
+        if (Math.abs(cast.polar.angle - mana.hue) < 45) {
+            console.log(mana);
+            spellDamage += mana.damage * cast.polar.magnitude;
+            let effect = Object.keys(mana.effect);
+            spellEffect[effect] = mana.effect[effect] * cast.polar.magnitude;
+        } else if (mana.hue == 180 && cast.polar.angle < -135) { // needed to correctly handle angles for STABILITY
+            console.log(mana);
+            spellDamage += mana.damage * cast.polar.magnitude;
+            let effect = Object.keys(mana.effect);
+            spellEffect[effect] = mana.effect[effect] * cast.polar.magnitude;
+        }
+    });
+    target.hp -= Math.floor(spellDamage);
+    Object.entries(spellEffect).forEach(element => {
+        //console.log(element);
+        if (element[0] == 'burn') {
+            target.effects.push(element[0], Math.floor(element[1]));
+        } else if (Math.random() <= element[1]) {
+            target.effects.push(element[0]);
+        }
+    });
 }
 
 function playerCastsSpell() {
     let spell = resolveSpell(player.attack);
     displaySpellEffect('player', spell);
-    counterEnemyMana();
-    applyEnemySpell();
+    counterMana(player,enemy);
+    applySpell(enemy, player);
+    playerHP.innerHTML = `${player.hp}`;
+    playerEffects.innerHTML = `${player.effects.join('<br>')}<br>`;
     pickEnemyAction();
     newRound();
 }
